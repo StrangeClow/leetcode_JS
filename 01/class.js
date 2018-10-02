3ef29f78c014fb8588bdd339ff57d5a8c89b8e0d
StrangeClow <806343718@qq.com>
2018-10-02 14:32:06 +0800

    :pencil:

---
+++ 01/class.js
@@ -0,0 +1,17 @@
+class point  {
+    constructor(x,y) {
+          this.x = x;
+          this.y =y;
+    }
+    static distance (a,b) {
+       const dx = a.x - b.y;
+       const dy = a.y - b.y;
+
+       return Math.max(dx,dy);
+    }
+}
+
+const p1 = new point(5,5);
+const p2 = new point(10,10);
+
+console.log(point.distance(p1,p2));
