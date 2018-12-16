9314f54f736f013374078dc1d46302092f076fba
StrangeClow <806343718@qq.com>
2018-12-16 15:19:40 +0800

    :bug:

--- backJs/01/test.js
+++ backJs/01/test.js
@@ -1,4 +1,36 @@
 (function() {
     console.log(arguments[0]+arguments[1]);
-})(12,89)
+})(12,890)
 
+
+var arr = [{
+       key: '01',
+       value: '乐乐'
+    }, 
+    {
+       key: '02',
+      value: '博博'
+    }, 
+    {
+       key: '03',
+       value: '淘淘'
+       },
+       {
+          key: '04',
+          value: '哈哈'
+       },
+       {
+          key: '01',
+      value: '乐乐'
+      }];
+
+      let c=  [99,88,77,10,0.9,88,33,77,0.9,1,1]
+var hash = {};
+arr = arr.reduce(function(item, next) {
+    hash[next.key] ? '' : hash[next.key] = true && item.push(next);
+    return item
+}, [])
+console.log(arr);
+
+console.log([...new Set(JSON.stringify(arr))]);
+console.log([...new Set(c)]);
