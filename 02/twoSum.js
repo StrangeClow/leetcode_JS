078d22ae8d0d4e5fc36b5754bfb77ae6acc6bc13
StrangeClow <806343718@qq.com>
2019-05-29 22:05:54 +0800

    1.两数之和

--- 02/twoSum.js
+++ 02/twoSum.js
@@ -14,3 +14,15 @@
     });
     return result;
  }
+
+
+// 双层循环遍历数据 让目标值等于 i+j   返回即可
+ var secondTwoSum = function(nums, target) {
+    for(let i = 0; i < nums.length; i++) {
+      for(let j = i+1;j<nums.length;j++) {
+           if(target == nums[i] + nums[j]) {
+               return [i,j]
+           }
+      }
+    }
+ }
