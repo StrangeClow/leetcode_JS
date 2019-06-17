4a140d4a2e992e044f5734f61fec6c527c69f9e7
StrangeClow <806343718@qq.com>
2019-06-17 22:45:01 +0800

    1.合并有序链表

--- 02/mergeTwoList.js
+++ 02/mergeTwoList.js
@@ -4,3 +4,21 @@
  * 
  * 
  */
+
+ var mergeTwoLists = function(l1,l2) {
+    let l3 = new ListNode(-1),
+    c3 = l3;
+    while(l1.val !== null && l2.val !== null) {   // 判断给定有序链表是否为空
+        if(l1.val <= l2.val) {
+            c3.next = l1;
+            l1 = l1.next;
+        } else {
+            c3.next = l2;
+            l2 = l2.next;
+        }
+        c3 = c3.next;
+    }
+    //循环完某一链表后 将另一链表剩下的部分直接加入到l3
+    c3.next = (l1===null) ? l2 : l1;
+    return l3.next;
+ },
