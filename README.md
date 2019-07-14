2c40dad2e92af12b106334d7c754f2c97e387073
StrangeClow <806343718@qq.com>
2019-07-14 21:19:04 +0800

    最大的数

--- README.md
+++ README.md
@@ -28,6 +28,7 @@
  23 | [将有序数组转换为二叉搜索树]() | 简单 | [JavaScript]() |  ✘
  24 | [杨辉三角]() | 简单 | [JavaScript](https://github.com/StrangeClow/leetcode_JS/blob/master/leetcode/pascals-triangle.js) |   √
  25 | [只出现一次的数字](https://leetcode.com/problems/single-number/) | 简单 | [JavaScript](https://github.com/StrangeClow/leetcode_JS/blob/master/leetcode/singleNumber.js) |  √
+ 26 | [最大数]() | 简单 | [JavaScript]() |  √
  
  
  
---
+++ leetcode/largestNumber.js
@@ -0,0 +1,22 @@
+/**
+ *  给定一组非负整数 重新排列表他们的顺序使其成为最大的整数 
+ * 
+ * eg :input== [10,2]   output == 210
+ *     input== [3,30,34,5,9]  ouput == 9534330
+ * 
+ * 
+ * @param {*} nums
+ * @returns
+ */
+var largestNumber = function(nums) {
+  // 首先判断特殊情况 处理数据边界问题
+  if(nums.every(item=> n === 0)) {
+    return '0'
+  }
+  else {
+      return nums.sort((a,b)=> {
+          [a,b] = [a.toString(),b.toString()]     // 解构赋值 交换变量
+          return parseInt(a + b) > parseInt(b + a) ? -1 : 1
+      }).join('')
+  }
+}
