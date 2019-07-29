eae13f18dd103ee8be6e53538ff6efe8a6e0badb
StrangeClow <806343718@qq.com>
2019-07-29 20:37:24 +0800

    扁平化数组

---
+++ 30MinutesCode/ARRAY/all.js
@@ -0,0 +1,4 @@
+const all = (arrr, fn= Boolean) => arrr.every(fn);   
+
+
+// 利用了es6的every   
--- 30MinutesCode/ARRAY/deepFlatten.js
+++ 30MinutesCode/ARRAY/deepFlatten.js
@@ -10,13 +10,26 @@
 //      [].concat(...arr.map(v => (Array.isArray(v) ? deepFlatten(v) : v)));
 // }
 
-const aa = function(arr) {
-    if(Array.isArray(arr)) {
-        console.log('hhhha');
-    }
-}
+
  const deepFlatten = arr => [].concat(...arr.map(v => (Array.isArray(v) ? deepFlatten(v) : v)));
 
- console.log('变种扁平化数组==>',aa([1, [2], [[3], 4], 5]));
+var deepFlattenTwo = function(arr) {
+  var result = []
+  for (let index = 0; index < arr.length; index++) {
+      if(Array.isArray(arr[index])) {
+         result = result.concat(deepFlattenTwo(arr[index]))    // 递归调用自身
+      } else {
+          result.push(arr[i])   // 不是数组  证明是单个数据  push添加
+      }
+  }
+  return result
+}
+
+
+
+
+
+
+ console.log('变种扁平化数组==>',deepFlattenTwo([1, [2], [[3], 4], 5]));
 console.log(deepFlatten([1,[9,[88,[485]]], [2], [[3], 4], 5]))
     
