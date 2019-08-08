086341f5f06d14746f055bab28152861aeacaf0b
StrangeClow <806343718@qq.com>
2019-08-08 21:55:22 +0800

    1.二叉树的最大深度

---
+++ leetcode/maxDepth.js
@@ -0,0 +1,16 @@
+/**
+ *  题意： 给定一二叉树，求取其的最大深度
+ *   二叉树的深度为根节点到最远子节点的最长路径上的节点数
+ *
+ * @param {*} root
+ * @returns
+ */
+const maxDepth = function(root) {
+  // 边界情况 
+  if(!root) {
+     return 0
+  }
+  var left_depth = maxDepth(root.left);
+  var right_depth = maxDepth(root.right);
+  return Math.max(left_depth, right_depth)+1;  // 求取左右节点中的最大值 再加上根节点
+}
