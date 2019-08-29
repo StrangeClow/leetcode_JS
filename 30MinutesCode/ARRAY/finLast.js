50ae694774165b5454ec9855a84c207e6fe50449
StrangeClow <806343718@qq.com>
2019-08-29 22:03:57 +0800

    :heart:

---
+++ 30MinutesCode/ARRAY/finLast.js
@@ -0,0 +1,10 @@
+/**
+ *Returns the last element for 
+ which the provided function returns a truthy value.
+ *
+ * @param {*} arr
+ * @param {*} fn
+ */
+const findLast = (arr, fn)=> {
+    arr.filter(fn),pop();
+}
