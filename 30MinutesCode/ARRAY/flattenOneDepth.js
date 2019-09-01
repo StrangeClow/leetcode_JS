df30941f56c2bcb1d412ea5c0e593b2e441ef599
StrangeClow <806343718@qq.com>
2019-09-01 10:11:24 +0800

    :heart:

---
+++ 30MinutesCode/ARRAY/flattenOneDepth.js
@@ -0,0 +1,11 @@
+/**
+ *  解析一层
+ *
+ * @param {*} arr
+ * @param {number} [depth=1]
+ */
+const flattenOneDepth = (arr, depth =1) => {
+    arr.reduce((a,v)=>{
+        a.concat(depth > 1 && Array.isArray(v) ? flattenOneDepth(v, depth -1): v)
+    },[])
+}
