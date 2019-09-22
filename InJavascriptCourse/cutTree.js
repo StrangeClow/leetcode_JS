5f2b921448c0da35fa0f539863704525d7e10450
StrangeClow <806343718@qq.com>
2019-09-22 14:27:47 +0800

    剪枝问题

---
+++ InJavascriptCourse/cutTree.js
@@ -0,0 +1,38 @@
+/**
+ *   剪枝问题 
+ *      在一条马上路，有很多树，树的高度不一，现在要统一剪枝到高度h,大于h,需剪刀h,小于则不变，所有
+ *    树减掉的总长度为C, 现在要使C>某个值的情况下(MM) 使h最大 ，求怎么确定h
+ *
+ * @param {*} list
+ * @param {*} MM
+ * @param {*} range
+ * @returns
+ */
+function cutTree(list,MM,range) {
+   if(list.length == 0) {
+       return 0
+   }
+   let start = 0,
+       end = Math.max(...list)   // 去list的最大值
+    while(start <= end) {
+      const middle = start + ((end - start) >> 1)
+      let res = 0
+      for(let i = 0; i < list.length; i++) {
+         if(list[i] < middle) {
+             res = res + list[i] - middle
+         }
+      } 
+      if(res > MM) {
+         if(res - MM <= range) {
+            return middle
+         }
+         end = middle - range
+      }
+      else {
+          start = middle + range
+      }
+    }   
+    return -1
+}
+
+// 主要是思路
