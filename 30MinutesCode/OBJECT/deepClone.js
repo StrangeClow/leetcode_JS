0f0a185a15e5b88c2d5889020e908e6225609812
StrangeClow <806343718@qq.com>
2019-09-22 21:53:47 +0800

    :heart:

--- 30MinutesCode/OBJECT/deepClone.js
+++ 30MinutesCode/OBJECT/deepClone.js
@@ -17,3 +17,23 @@ const deepClone = (obj) => {
      ? Array.from(obj)
      : clone
 }
+
+
+
+function deepCloneTwo(source) {
+   if(typeof source !== "Object" && source == null) {   //如果当前source不是一个对象则返回自身
+         return source
+   }
+   var target = Array.isArray(source) ? [] : {}
+   for (var key in source) {    // 循环对象的属性
+      if(Object.prototype.hasOwnProperty.call(source,key)) {
+         if(typeof source[key] == "Object" && source[key] !== null) {
+            target[key] = deepCloneTwo(source[key])
+         }
+         else {
+            target[key] = source[key]
+         }
+      }
+   }
+   return target
+}
--- InJavascriptCourse/toSoluteCourse.js
+++ InJavascriptCourse/toSoluteCourse.js
@@ -7,5 +7,28 @@
  *                   B. 匿名自执行包裹异步操作 加一层闭包 
  *                   C， 用一个临时变量替换
  * 
+ *       
+ *     
+ *    原因： 由于作用域链的机制，闭包只能取得内部函数的最后一个值，这样的结果是
+ *    如果内部函数在一个循环中，那么变量的值始终未最后一个值。
+ * 
  * 
  */
+
+ var data = []
+     for (var index = 0; index < 3; index++) {
+         data[i] = function() {
+             console.log(i);
+         }
+     }
+
+
+ // 改进
+ 
+  for (var index = 0; index < 3; index++) {
+     data[i] = (function(num){
+        return function() {
+            console.log(num);
+        }
+     })(i)
+  }
