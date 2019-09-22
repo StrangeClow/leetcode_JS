9e2378a6a299839dc5e2d41bc211a85b9ba10f23
StrangeClow <806343718@qq.com>
2019-09-22 13:53:30 +0800

    flatten

---
+++ 30MinutesCode/ARRAY/flatten.js
@@ -0,0 +1,44 @@
+
+
+/**
+ *  
+ * 
+ * @param {*} arr   arr表示平铺的数组
+ * @param {number} [depth=1]   depth代表平铺的深度
+ */
+const flatten = (arr, depth =1) => 
+    depth != 1
+    ? arr.reduce((prev,curr) => prev.concat(Array.isArray(curr) ? flatten(curr, depth -1): curr),[])
+    : arr.reduce((prev, curr) => prev.concat(curr),[])
+
+
+     console.log('扁平化数组信息===>',flatten([1, [2,[5,6,[8,9]]], 3, 4],2));
+
+
+const flattenOne = (arr, depth =1) => {
+    if(depth == 1) {
+        console.log('arr===>',arr);
+      arr.reduce((prev,curr)=> {
+          console.log('prev===>',prev,curr);
+          prev.concat(curr)
+      },[])
+    } else {
+        arr.reduce((prev,curr,index,arrInfo)=> {
+            console.log('prev,curr,index,arrInfo',index,arrInfo);
+            prev.concat(Array.isArray(curr) ? flattenOne(curr,depth -1): curr)
+        },[])
+    }
+}
+
+// console.log('扁平化数组===>',flattenOne([1, [2,[5,6,[8,9]]], 3, 4],2));
+
+
+// const aa = (arrr)=> 
+// console.log('arr',arrr);
+//      arrr.reduce((prev,curr,index,arrInfo)=> {
+//         console.log(prev,curr,index,arrInfo);
+//         return prev+ curr
+//     },[])
+
+
+// console.log('aa=====>',aa([1,2,3,4,5,6]));
