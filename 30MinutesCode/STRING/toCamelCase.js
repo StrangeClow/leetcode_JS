ca57c44bebebb33c2027d07973f744d9619aafd2
StrangeClow <806343718@qq.com>
2019-10-10 21:53:33 +0800

    转换为驼峰格式

---
+++ 30MinutesCode/STRING/toCamelCase.js
@@ -0,0 +1,17 @@
+/**
+ *    将字符串转换为驼峰命名的格式  
+ * 
+ *    1. 首先判断边界情况 是否是字符串
+ *    2. 正正则匹配首字母 toUpperCase() 以及toLowerCase()
+ *    3.  转化为数组
+ *
+ * @param {*} str
+ * @returns
+ */
+const toCamelCase = str => {
+    let s = str && 
+        str.match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+        .map(x=> x.slice(0,1).toUpperCase() + x.slice(1).toLowerCase())
+        .join('')
+        return s.slice(0,1).toLowerCase() + s.slice(1)
+}
