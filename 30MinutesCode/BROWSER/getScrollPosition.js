3202977f799bd7c4cdb17b28005694f06855e568
StrangeClow <806343718@qq.com>
2019-10-29 20:19:51 +0800

    获取滚动位置

---
+++ 30MinutesCode/BROWSER/getScrollPosition.js
@@ -0,0 +1,9 @@
+/**
+ *   返回当前页面滚动的位置  利用BOM相关的一些特性去解决     
+ *
+ * @param {*} [el=window]
+ */
+const getScrollPosition = (el = window) => ({
+    x: el.pageOffest !== undefined ? el.pageOffset : el.scrollLeft,
+    y: el.pageYOffset !== undefined ? el.pageYOffset : el.scrolltTop
+})
