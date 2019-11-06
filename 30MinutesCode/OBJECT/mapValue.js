177fe30d2d42bcf1600f2316b9059957a8b68f7e
StrangeClow <806343718@qq.com>
2019-11-06 21:55:00 +0800

    键值映射

---
+++ 30MinutesCode/OBJECT/mapValue.js
@@ -0,0 +1,16 @@
+/**
+ *    使用提供的对象相同的键创建一个对象 对应值是通过函数迭代生成的
+ * 
+ *   reduce
+ * 
+ *   object.keys()
+ *
+ * @param {*} obj
+ * @param {*} fn
+ */
+const mapValues = (obj, fn) =>
+  Object.keys(obj).reduce((acc,k) =>{
+      accc[k] = fn(obj[fn],k,obj);
+      return acc;
+  }  
+  ) 
