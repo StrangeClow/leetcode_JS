e4c0125df3626ca869c211a7754a5e199dd11e49
StrangeClow <806343718@qq.com>
2019-12-03 21:49:36 +0800

    :heart:

---
+++ 30MinutesCode/ARRAY/pullAtIIndex.js
@@ -0,0 +1,17 @@
+/**
+ *     删除数组中指定索引的值   
+ *      
+ * filter过滤数组指定值  includes进行判断  
+ *
+ * @param {*} arr
+ * @param {*} pullArr
+ * @returns
+ */
+const pullAtIndex = (arr, pullArr) => {
+    let removed = [],
+        pulled = arr.map((v, i)=>(pullArr.includes(i) ? removed.push(v) : v))
+        .filter((v, i)=> !pullArr.includes(i))
+        arr.length = 0
+        pulled.forEach(v=> arr.push(v))
+        return removed
+}
