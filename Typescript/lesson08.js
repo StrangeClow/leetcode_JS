a239b1e172a18b78ef9dc9b59108d9675d448a97
StrangeClow <806343718@qq.com>
2019-12-08 19:38:20 +0800

    type

---
+++ Typescript/lesson08.js
@@ -0,0 +1,10 @@
+//   声明文件 
+function declare() { }
+var declare = /** @class */ (function () {
+    function declare() {
+    }
+    return declare;
+}());
+(function (declare) {
+})(declare || (declare = {}));
+namespace; // 声明命名空间
---
+++ Typescript/lesson08.ts
@@ -0,0 +1,12 @@
+//   声明文件 
+
+    //  当使用第三方库时，我们需要引用它的声明文件 才能获得对应的代码补全 接口提示灯内容
+
+
+    // 新语法索引
+
+    declare var;  // 声明全局变量
+    declare function // 声明全局方法
+    declare class  // 声明全局类
+    declare enum ;   // 声明枚举
+    declare namespace ; // 声明命名空间
