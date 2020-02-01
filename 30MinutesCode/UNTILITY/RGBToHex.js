4d7d5644f774c964f47e7eef16a5add8782db347
StrangeClow <806343718@qq.com>
2020-02-01 19:01:05 +0800

    :fire:

---
+++ 30MinutesCode/UNTILITY/RGBToHex.js
@@ -0,0 +1,15 @@
+/**
+ *   rgb值转换为简单的颜色代码 
+ * 
+ *      1. <<   按位左移运算符 
+ *      2. toString(16)将给定的RGB(255,123,90)转换为16进制字符串
+ *      3. 使用string.padStart(6,'0')得到六位的16进制值  (其中包含ABCDEF)
+ *
+ * @param {*} r
+ * @param {*} g
+ * @param {*} b
+ */
+const RGBToHex = (r, g, b) =>
+    ((r << 16) + (g << 8) + b).
+    toString(16).
+      padStart(6,'0');
