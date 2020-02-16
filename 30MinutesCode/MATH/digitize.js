6efbd359f7cdcb3dd49afec13f26f6d4275b752e
StrangeClow <806343718@qq.com>
2020-02-16 10:56:58 +0800

    :heart: sun

---
+++ 30MinutesCode/MATH/digitize.js
@@ -0,0 +1,21 @@
+/**
+ *   把一串数字转化为一个数组 
+ *
+ * @param {*} n
+ */
+const digitize = n => 
+
+  // ... 展开运算符构建一个新的数组
+  [... n].map((item)=> {
+
+      // parseInt转化为整型数据
+      parseInt(item)
+
+  })
+
+
+
+// eg :
+//   input : 12345
+//   output: [1,2,3,4,5]
+
