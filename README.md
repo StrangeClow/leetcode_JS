912976ee959658819054a6ae22e65e898ae860cf
StrangeClow <806343718@qq.com>
2020-02-19 15:14:21 +0800

    :heart:  good luck

--- README.md
+++ README.md
@@ -47,7 +47,8 @@
  42 | [字符串中的单词数](https://leetcode.com/problems/const-segments/) | 中等 | [JavaScript](https://github.com/StrangeClow/leetcode_JS/blob/master/leetcode/contSegments.js) |   √
  43 | [特殊等价字符串组]() | 中等 | [JavaScript]() |   ✘
  44 | [各位相加]() | 简单 | [JavaScript]() |   ✘
- 44 | [数组的相对排序]() | 中等 | [JavaScript]() |   ✘
+ 45 | [数组的相对排序]() | 中等 | [JavaScript]() |   ✘
+ 46 | [快乐数]() | 简单 | [JavaScript]() |   ✘
  
  
  
---
+++ leetcode/isHappyNumber.js
@@ -0,0 +1,26 @@
+/**
+ * 快乐数 
+ *    定义： 对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和，然后重复
+ * 这个过程直到这个数变为1，也可能是无限循环但始终变不到1。如果可以变为1，那这个数就是快乐数
+ *
+ * @param {*} num
+ */
+const isHappyNumber = function(n) {
+
+     let set = new Set()
+     let sum = 0
+     n = ''
+     while(sum !== 1) {
+         sum = 0
+         for (let index = 0; index < n.length; index++) {
+            sum+=n[i] * n[i]
+             
+         }
+         if(set.has(sum))   return false
+
+         n = sum+''
+         set.add(sum)
+         
+     }
+     return true
+}
