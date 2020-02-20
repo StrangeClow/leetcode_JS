d97169bb3302b6a64eae7ed6b618b441e1ede865
StrangeClow <806343718@qq.com>
2020-02-20 16:07:01 +0800

    lucking

--- 30MinutesCode/DATE/DayOfYear.js
+++ 30MinutesCode/DATE/DayOfYear.js
@@ -1,6 +1,10 @@
 /**
- *
+ *  返回一个结介于1-366之间的整数 该整数表示日期时间或表达式中的一年中的连续日期 
  *
  * @param {*} date
  */
-const dayOfYear = date => Math.floor(date - new Date(date.getFullyeaar(),0 ,0) / 1000/ 60 /60 /60 / 24)
+const dayOfYear = date => Math.floor(date - new Date(date.getFullYear(),0 ,0) / 1000/ 60 /60 /60 / 24)
+
+
+
+console.log('一年中的第几天..',dayOfYear(new Date('2020-02-20')));
--- 30MinutesCode/DATE/getDaysDiffBetween.js
+++ 30MinutesCode/DATE/getDaysDiffBetween.js
@@ -1,9 +1,16 @@
 /**
+ *     获取两个日期之间相差的天数  
+ * 
  *   注意计算规则以及各个参数的要求
  *
  * @param {*} beginDate
  * @param {*} endDate
  */
 const getDaysDiffBetween =  (beginDate, endDate) => {
-    (endDate - beginDate) / (1000*3600*24)
+  
+  return  (endDate - beginDate) / (1000 * 3600 * 24);
 }
+
+
+ 
+ console.log(getDaysDiffBetween(new Date('2017-12-13'), new Date('2017-12-22')));
