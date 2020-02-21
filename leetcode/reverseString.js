f917f07ca2ae79bb77028ed815534f16fd9a39f7
StrangeClow <806343718@qq.com>
2020-02-21 15:29:36 +0800

    2020

--- leetcode/reverseString.js
+++ leetcode/reverseString.js
@@ -1,11 +1,40 @@
 /**
  *   反转字符串
  *
- * @param {*} ss
+ * @param {*} str
  * @returns
  */
-const reverseString = function(ss) {
-    // 表达式先判断
-    // 在reduce累加求和
-    return !s ? s : s.split('').reduce((prev,curr)=> prev+curr)
+
+const reverseString2 = function(str) {
+    if(! str)   return false
+
+    if( str.length === 1) return str
+    
+    return str.split('').reverse().join('')
 }
+
+
+console.log(reverseString2('hello world'));
+
+
+
+
+/**
+ *
+ *
+ * @param {*} str
+ */
+const reverseString = function(str) {
+   if(! str) return  false
+
+   let result = ''
+   let count = str.length - 1
+   while(count >= 0) {
+       result += str[count]
+       count -= 1
+   } 
+   return result   
+}
+
+
+console.log(reverseString('reverseString'));
