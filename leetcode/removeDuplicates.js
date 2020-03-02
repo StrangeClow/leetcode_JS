e8e39ec4c2314791fc3207aec89a0c4c0181b3fa
StrangeClow <806343718@qq.com>
2020-03-02 17:22:14 +0800

    :bug:  202003021722 :bug:

--- leetcode/removeDuplicates.js
+++ leetcode/removeDuplicates.js
@@ -11,15 +11,55 @@
   * 思路 ：重复元素一定是数组中相邻的元素 我们可以通过遍历数组 找出相邻的相同项
  */
 
+ //  这个返回了移除后的数据项  
  var removeDuplicates = function(num) {
    // 基础for循环
     for(let i = 0; i< num.length; i++) {
       if(num[i] == num[i+1]) {
-          num.splice(i,1)    // 注意splice的用法 不同的传参的意义
-          i--;  // 移除之后 i-1  防止跳过
+          //  此处Array.splice操作相当于改变了数组  对题意有点违背 
+          num.splice(i,1)    // 注意splice的用法 不同的传参的意义   
+          // 移除之后 i-1  防止跳过 
+          i--
       }
     }
     return num  
- },
+ }
 
+
+ let n1 = [1,3,4,5,8,8,9,10,10,11]
+ console.log('移除排序数组的重复项====>',removeDuplicates(n1))
+
+
+
+ /**
+  *   
+  *    不依赖额外的资源或者依赖少数的额外资源 
+  *  
+  *       定义一个变量存储数组长度 数组的长度在处理后会发生变化 
+  *
+  * @param {*} nums
+  * @returns
+  */
+ const removeDuplicates2 = function(nums) {
+    
+    // 边界检测 
+    if(nums.length == 0) return 0
+    if(nums.length == 1) return nums
+    
+    // 定义初始数组长度 
+    let slow = 0
+    for(let fast = 0; fast< nums.length; fast++) {
+      console.log('nums[slow]===>',nums[slow],nums[fast])
+      // nums[fast]循环数组的值 若不等 指针前移
+      if(nums[slow] !== nums[fast]) {
+        slow ++
+        nums[slow] = nums[fast]
+      }
+    }
+    return slow + 1
+
+ }
  
+
+let n2 = [0,0,1,1,2,3,4,5,5,5,6]
+console.log(removeDuplicates2(n2))
--- leetcode/removeElement.js
+++ leetcode/removeElement.js
@@ -11,5 +11,46 @@ var removeElement = function(nums,val) {
            i--;  // i递减  删除之后向前移动位置
        }
    }
+   console.log('nums==>',nums)
    return nums.length
 } 
+
+let n1 = [1,2,4,6,8]
+let t1 = 4
+
+console.log('原地移除元素===>',removeElement(n1,t1))
+
+
+
+/**
+ *
+ *
+ * @param {*} nums
+ * @param {*} target
+ * @returns
+ */
+const removeElement2 = function(nums, target) {
+   
+    // 先对数组排序 
+    nums.sort((a,b) => {
+        return a -b
+    })
+    console.log('nums排序后',nums)
+    // indexOf某个字符首次出现在字符串中的位置 
+    let index= nums.indexOf(target) 
+    let i = index
+    // 若不存在 
+    if(index != -1) {
+       while(nums[i] == target) {
+          i++
+       }
+       let len = i -index
+       nums.splice(index, len)
+    }
+    return nums.length
+}
+
+let n2 = [1,2,4,6,8,3,5]
+let t2 = 8
+
+console.log('原地移除元素===>',removeElement2(n2,t2))
