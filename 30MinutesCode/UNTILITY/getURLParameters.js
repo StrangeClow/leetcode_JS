7e683f33f4e1e286bf7d4e2ba1282f23f1b6356b
StrangeClow <806343718@qq.com>
2020-03-04 16:02:11 +0800

    :fire:  202003041604

--- 30MinutesCode/UNTILITY/getURLParameters.js
+++ 30MinutesCode/UNTILITY/getURLParameters.js
@@ -3,7 +3,34 @@
  *
  * @param {*} url   url参数
  */
-const getURLParameters = url => 
-  url .match(/([^?=&]+)(=[^&]*)/)    // 正则表达式匹配
-  .reduce((a,v)=>
-  ((a[v.slice(0,v.indexOf('='))] = v.slice(v.indexOf("=")+1)),a),{})
+
+
+
+
+  let a1 = 'https://music.163.com/#/discover/toplist?id=2006508653'
+
+
+  let a2 = 'http://url.com/page?name=Adam&surname=Smith'
+
+
+
+
+
+
+    const getURLParameters3 = function(url) {
+       
+      // 正则匹配  匹配url路径的参数间隔符  ？ = & 等
+      let regexp = /([^?=&]+)(=([^&]*))/g
+      // 正则匹配后的参数值
+      let regUrl = url.match(regexp)
+      let res = regUrl.reduce((prev, curr) => 
+          (
+            // slice（start，end）从已有的数组中返回选定的元素 
+           (prev[curr.slice(0, curr.indexOf('='))] = curr.slice(curr.indexOf('=') + 1))
+           ,prev) 
+        ,{})
+      return res
+    }
+
+console.log('解析url参数',getURLParameters3(a2));
+
--- 30MinutesCode/UNTILITY/validateNumber.js
+++ 30MinutesCode/UNTILITY/validateNumber.js
@@ -4,5 +4,14 @@
  * @param {*} n
  */
 const validateNumber = n=> {
-    !isNaN(parseFloat(n)) && isFinite(n) && Number(n) == n;
+  return  !isNaN(parseFloat(n)) && isFinite(n) && Number(n) == n;
 }
+
+
+console.log(validateNumber(123));
+console.log(validateNumber(true));
+console.log(validateNumber('hello'));
+console.log(validateNumber({a:"hello"}));
+console.log(validateNumber(null));
+console.log(validateNumber(undefined));
+// console.log(validateNumber(NAN));
