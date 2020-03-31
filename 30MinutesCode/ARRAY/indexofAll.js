f8829316fa162a7ab044c0766bf787b0a99f0cff
StrangeClow <806343718@qq.com>
2020-03-31 14:27:34 +0800

    :fire:  202003311427

--- 30MinutesCode/ARRAY/indexofAll.js
+++ 30MinutesCode/ARRAY/indexofAll.js
@@ -8,6 +8,11 @@
 const indexofAll = (arr, val) => {
     const ifIncludes = []
     //  循环判断是否有值相等  
-    arr.forEach((el, i) => el === val && ifIncludes.push(i))
+    arr.forEach((el, i) => el === val && ifIncludes.push(i))    // 存储匹配元素的索引 
     return ifIncludes
 }
+
+
+
+let n1 = [1,2,3,4,5,6]
+console.log(indexofAll(n1,5));
---
+++ 30MinutesCode/ARRAY/initial.js
@@ -0,0 +1,10 @@
+/**
+ *  删除数组中的最后一个元素 
+ *
+ * @param {*} arr
+ * @returns
+ */
+const initial = function(arr) {
+  // slice()  不同参数   
+  return arr.slice(0, -1)
+}
