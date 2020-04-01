f659b1a07f4cd5cb6ae4cbe6822ed743406886c0
StrangeClow <806343718@qq.com>
2020-04-01 10:48:36 +0800

    :fire:  202004011048   luck to all

--- 30MinutesCode/ARRAY/reducedFilter.js
+++ 30MinutesCode/ARRAY/reducedFilter.js
@@ -10,4 +10,62 @@ const reduceFilter = (data, keys, fn) =>
         keys.reduce((acc,key)=> {
             acc[key] = el[key]
             return acc
-        },{}))
+        },{})
+    )
+
+
+/**
+ *
+ *
+ * @param {*} data 原数据集合
+ * @param {*} keys 数据集合键
+ * @param {*} fn  过滤函数
+ * @returns
+ */
+const reduceFilter2 = function(data, keys, fn) {
+
+  // 拿到过滤后的数据信息
+  let filterData = data.filter(fn)   // filter过滤函数 fn()
+
+  console.log(filterData);
+
+  return filterData.map((item,index)=> {
+
+    console.log('过滤索引===',index);
+
+    return  keys.reduce((prev,curr)=> {     // keys指代根据某条件过滤
+       
+        prev[curr] = item[curr]
+     
+        console.log('prev后',prev[curr],curr);
+
+        return prev
+
+      },{})
+   })
+}    
+
+
+
+const data = [
+  {
+    id: 1,
+    name: 'john',
+    age: 24
+  },
+  {
+    id: 2,
+    name: 'bob',
+    age: 27
+  },
+  {
+    id: 3,
+    name: 'laker',
+    age: 38
+  },
+]    
+
+console.log(reduceFilter2(data, ['id','name'], item => item.age > 24));
+
+
+console.log(reduceFilter(data, ['id','name'], item => item.age > 24));
---
+++ 30MinutesCode/ARRAY/tail.js
@@ -0,0 +1,45 @@
+/**
+ *  返回剔除第一个元素后的数组 
+ *
+ * @param {*} arr
+ * @returns
+ */
+const tail = function(arr) {
+  if(arr.length > 1) {
+    return arr.slice(1)
+  }
+   else {
+       return arr
+   }
+}
+
+
+const tail3 = arr => (arr.length > 1 ? arr.slice(1) : arr)
+
+
+/**
+ *
+ *
+ * @param {*} arr
+ * @returns
+ */
+const tail4 = function(arr) {
+  return arr.filter((item,index)=> {
+      // 拿索引
+      if(index !== 0) {
+        return item
+      }
+  })
+}
+
+let n1 = [1,2,3,4,5,6,7]
+let n2 = [2]
+let n3 = [2,343,4656,7787878]
+let n4 = [99,66.90,6767,2323,12345,67]
+
+console.log(tail(n1));
+console.log(tail(n2));
+console.log(tail(n3));
+console.log(tail3(n3));
+console.log(tail4(n3));
+console.log(tail4(n4));
