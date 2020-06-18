9e2b4827aba409ddc5dfb5e06d1ebd934c05ebcd
StrangeClow <806343718@qq.com>
2020-06-18 11:30:39 +0800

    :bug: 202006181130  单链表

--- leetcode/getKthFromEnd.js
+++ leetcode/getKthFromEnd.js
@@ -1,3 +1,31 @@
-const getKthFromEnd = function(head, k) {
+/**
+ *  链表中倒数第k个节点 
+ * 
+ *      输入一个链表 输出该链表中倒数第k个节点  
+ * definition for singly-linked list
+ * function ListNode(val) {
+ *    this.val = val
+ *    this.next = null
+ * }
+ *
+ * @param {*} head
+ * @param {*} k
+ * @returns
+ */
+const getKthFromEnd = function(head, k) {      // 双指针 
+  
+    let p = head, q = head    // 声明p q两个指针 
 
+    let i = 0
+
+    while(p) {
+        if(i > k) {
+            q = q.next   // 让p先走k步
+        }
+        p = p.next
+        i++
+    }
+    return i < k ? null : q
 }
+
+
